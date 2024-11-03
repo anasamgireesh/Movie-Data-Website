93d7febb973c4f1aaa85f15f448a5c50913c47d3
Gireesh <167363264+anasamgireesh@users.noreply.github.com>
2024-11-03 12:53:29 +0530

    Update Moviecard.jsx

--- Moviecard.jsx
+++ Moviecard.jsx
@@ -15,27 +15,5 @@ function Moviecard( props) {
 
 export default Moviecard;
 
-//  background-image: url("https://m.media-amazon.com/images/M/MV5BNWE5MGI3MDctMmU5Ni00YzI2LWEzMTQtZGIyZDA5MzQzNDBhXkEyXkFqcGc@._V1_SX300.jpg");
 
-// .emoticon{
-//   margin: 0.2rem;
-//   height: 1.6rem;
-//   width: 1.6rem;
-//   background-color: #948b8b;
-//   border-radius: 0.5rem;
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-// }
 
-// .moviecard{
-//   height: 20rem;
-//   width: 10rem;
-//   border-radius: 1rem;
-//   background-color: aqua;
-//   transition: ease-in-out 0.2s;
-//   display: flex;
-//   background-size: cover;
-//   background-position: center;
-//   color: white;
-// }
